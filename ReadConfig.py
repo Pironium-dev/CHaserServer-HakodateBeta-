5a178c7e5ef33bd0239ae9d2c0496d2c0c3f6300
piroping <pirogurama@gmail.com>
2022-09-19 15:49:48 +0900

    ReadConfigの中身作成 + etc

---
+++ ReadConfig.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+
+class ReadConfig:
+    def __init__(self):
+        self.names: list = ('CoolPort', 'HotPort')
+        self.config: Dict = {}
+        self.__Read()
+
+    def __Read(self):
+        with open('Config.dt', mode = 'r', encoding='utf-8') as f:
+            for name, item in zip(self.names, f.readlines()):
+                self.config[name] = item
+
+    def output_config(self):
+        return self.config
+
+
+if __name__ == '__main__':
+    test = ReadConfig()
+    print(test.output_config())
